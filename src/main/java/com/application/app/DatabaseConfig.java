d2b27021cdff5d37180dcffae600628e0921d8d2
Mateusz Ciszewski <matti288422@gmail.com>
2019-12-08 20:26:58 +0100

    przed sprawdzeniem nie wykrywa postgresdrivera

--- src/main/java/com/application/app/DatabaseConfig.java
+++ src/main/java/com/application/app/DatabaseConfig.java
@@ -4,6 +4,8 @@ import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class DatabaseConfig {
@@ -12,7 +14,8 @@ public class DatabaseConfig {
     private String dbUrl;
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws SQLException {
+//        DriverManager.getDriver("org.postgresql.Driver");
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
         return new HikariDataSource(config);
